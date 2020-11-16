e300cd78d7f7551a52745eeb66587fc96258eb98
Anel Husakovic <anel@mariadb.org>
2020-11-16 23:47:07 +0100

    MDEV-24200: Syntax error in mysqldump

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -10029,7 +10029,7 @@ static bool show_create_trigger_impl(THD *thd, Trigger *trigger)
   Protocol *p= thd->protocol;
   List<Item> fields;
   LEX_CSTRING trg_sql_mode_str, trg_body;
-  LEX_CSTRING trg_sql_original_stmt;
+  LEX_STRING trg_sql_original_stmt;
   LEX_STRING trg_definer;
   CHARSET_INFO *trg_client_cs;
   MEM_ROOT *mem_root= thd->mem_root;
@@ -10048,7 +10048,14 @@ static bool show_create_trigger_impl(THD *thd, Trigger *trigger)
 
   /* Prepare trigger "object". */
 
-  trigger->get_trigger_info(&trg_sql_original_stmt, &trg_body, &trg_definer);
+  trigger->get_trigger_info((LEX_CSTRING *)&trg_sql_original_stmt, &trg_body,
+                             &trg_definer);
+  if (!strcmp(&trg_sql_original_stmt.str[trg_sql_original_stmt.length -1], ";"))
+  {
+    --trg_sql_original_stmt.length;
+    trg_sql_original_stmt.str[trg_sql_original_stmt.length]= '\0';
+  }
+
   sql_mode_string_representation(thd, trigger->sql_mode, &trg_sql_mode_str);
 
   /* Resolve trigger client character set. */
